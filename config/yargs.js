f57ed87f2df5e1574dbf30eebe12951c8fa4672e
Juan Fernando Huañec Huacac <jhuanech@AUTRONIC.COM.PE>
2019-01-08 16:41:23 -0500

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+
+const descripcion = {
+	demand: true,
+	alias: 'd',
+	desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+	alias: 'c',
+	default: true,
+	desc: 'Marca como completado o pendiente la tarea'
+};
+
+const argv = require('yargs') //	 npm i yargs --save-dev	
+		.command('crear', 'Crear un elemento por hacer', {
+			descripcion
+		}) 
+		.command('actualizar', 'Actualizar el estado completado de una tarea', {
+			descripcion,
+			completado
+		})
+		.command('borrar', 'Borra una Tarea', {
+			descripcion
+		})
+		.help()
+		.argv;
+
+
+module.exports = {
+	argv
+};
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,78 @@
+
+
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+	let data = JSON.stringify(listadoPorHacer)
+
+	fs.writeFile(`db/data.json`, data, (err) => {
+	  if (err) throw new Error("No se pudo grabar", err);
+	});
+
+}
+
+const cargarBD = () => {
+	try{
+		listadoPorHacer = require('../db/data.json');
+	}catch(error) {
+		listadoPorHacer = [];
+	}
+};
+
+const crear = (descripcion) => {
+	cargarBD();
+	let porHacer = {
+		descripcion,
+		completado: false
+	};
+
+	listadoPorHacer.push(porHacer);
+	guardarDB();
+	return porHacer;
+}
+
+const getListado = () => {
+	cargarBD();
+	return listadoPorHacer
+}
+
+const actualizar = (descripcion, completado = true) => {
+	cargarBD();
+	let index = listadoPorHacer.findIndex(tarea => {
+		return tarea.descripcion === descripcion;
+	});
+
+	if (index >= 0) {
+		listadoPorHacer[index].completado = completado;
+		guardarDB();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+const borrar = (descripcion) => {
+	cargarBD();
+	
+	let nuevoListado = listadoPorHacer.filter(tarea => {
+		return tarea.descripcion !== descripcion;
+	})
+
+	if (listadoPorHacer.length === nuevoListado.length) {
+		return false
+	}else {
+		listadoPorHacer = nuevoListado;
+		guardarDB();
+		return true;
+	}
+
+};
+
+module.exports = {
+	crear,
+	getListado,
+	actualizar,
+	borrar
+}
